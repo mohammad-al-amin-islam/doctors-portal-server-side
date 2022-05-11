af5494a8b61b363fbeff403788c0c6b2eb899f17
mohammad-al-amin-islam <alamin.islambcpsc@gmail.com>
2022-05-11 19:59:59 +0600

    db connected and load data from db

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 5000;
+
+
+
+app.use(cors())
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.yuitj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+
+    try {
+        await client.connect();
+        const serviceCollection = client.db("doctor_portal").collection("service");
+
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+
+    }
+    finally {
+
+    }
+
+}
+
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+    res.send('Doctors portal server running successfully')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
